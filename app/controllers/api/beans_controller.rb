ea1ebc4596fe391857729061dc2f7c216f727565
Sean Earle <sean.r.earle@gmail.com>
2019-02-21 20:50:09 +1030

    Make the purchase beans command easier

--- app/controllers/api/beans_controller.rb
+++ app/controllers/api/beans_controller.rb
@@ -2,22 +2,14 @@ class Api::BeansController < ApiController
   def purchase
     @user = User.find_by(slack_id: params['user_id'])
 
-    with_location = /.*"(.*)".*"(.*)"/
-    without_location = /.*"(.*)"/
-
     weight, price = params['text'].split
+
+    name_and_location = params['text'].delete(weight).delete(price).strip
+
+    name, location = name_and_location.downcase.split('from').map(&:titleize)
 
     weight = Integer(weight.gsub('g', ''))
     price = Monetize.parse(price).cents
-
-    case params['text']
-    when with_location
-      name = $LAST_MATCH_INFO[1]
-      location = $LAST_MATCH_INFO[2]
-    when without_location
-      name = $LAST_MATCH_INFO[1]
-      location = ''
-    end
 
     @bean = Bean.create_with(location: location, team: @user.team).
       find_or_create_by(name: name)
